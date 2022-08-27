9f5e9aebe018541c5570cb421157abeb5642fc1e
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-08-27 16:39:42 +0700

    Upload modul 5 7

--- Section 7 -  Accessing API from Browser/Lesson 2 4.js
+++ Section 7 -  Accessing API from Browser/Lesson 2 4.js
@@ -2,10 +2,24 @@ const express = require('express')
 
 const app = express()
 
+/**
+ * Lesson 4 Start
+ */
 app.get('/weather', (req, res) => {
     // All query string key/value pairs are on req.query
-    res.send(`You provided ${req.query.address} as the address.`)
+    // res.send(`You provided ${req.query.address} as the address.`)
+    // JSON.parse(res.send(req.query.address))
+    res.send(
+        {
+            data: {
+                message: 'CREATE NEW USER: POST /user',
+                body: req.query.address
+            }
+        });
 })
+/**
+ * Lesson 4 End
+ */
 
 const greeter = (name = 'user', age) => {
     console.log('Hello ' + name)
@@ -13,9 +27,10 @@ const greeter = (name = 'user', age) => {
 greeter('Andrew') // Will print: Hello Andrew
 greeter()
 
-const transaction = (type, { label, stock = 0 } = {}) => {
+const transaction = (type, { label = 0, stock = 0 } = {}) => {
     console.log(type, label, stock)
 }
+
 transaction('order')
 
 app.listen(3000, () => {
